b334659e7b5be382b226c69eff9aa74624af7c0e
44.5.53.6.k <44.5.53.6.k@gmail.com>
2023-07-05 17:01:02 +0200

    Whisper updated

--- whisper/transcribe.py
+++ whisper/transcribe.py
@@ -1,3 +1,4 @@
+import asyncio
 import shutil
 
 import openai
@@ -58,6 +59,11 @@ def get_file_size(file_path):
     return os.stat(file_path).st_size / (1024 * 1024)  # size in MB
 
 
+def is_large(file_path):
+    # check if bigger than 20 mb
+    return get_file_size(file_path) > 20
+
+
 def split_audio(job_id, max_size):
     file_path = f"jobs/{job_id}/{job_id}.mp3"
     audio = AudioSegment.from_mp3(file_path)
@@ -76,8 +82,9 @@ def split_audio(job_id, max_size):
     return fragments
 
 
-async def convert_media_to_mp3(file_path, output_path):
+async def convert_media_to_mp3(file_path):
     input_format = os.path.splitext(file_path)[1][1:]
+    output_path = os.path.splitext(file_path)[0] + '.mp3'
     AudioSegment.from_file(file_path, input_format).export(output_path, format='mp3')
     return output_path
 
@@ -108,9 +115,71 @@ async def transcribe_and_save(input_path, output_path, output_format, fragment_i
     return content
 
 
-async def process_audio(file_path, prompt="", response_format="vtt", start_with=1, max_size=25):
-    # todo add other response formats
-    # This function creates separate folders for each job and transcribes the audio file
+async def process_job(job_id, prompt="", input_format="mp3"):
+    job_folder_path = folder_path + job_id
+    output_path = job_folder_path + '/' + job_id + '.json'
+    result = None
+
+    def merge_json_objects(json_objects):
+        merged_object = {"segments": []}
+        start_time_offset = 0
+
+        for json_object in json_objects:
+            for segment in json_object["segments"]:
+                start_time = segment["start"] + start_time_offset
+                text = segment["text"]
+
+                merged_object["segments"].append({
+                    "start": start_time,
+                    "text": text,
+                })
+
+                start_time_offset += segment["end"]
+
+        return merged_object
+
+    file_size = get_file_size(job_folder_path + '/' + job_id + '.' + input_format)
+
+    if is_large(job_folder_path + '/' + job_id + '.' + input_format):
+        print(f"File {job_id}.{input_format} is too large ({file_size} MB). Splitting...")
+        fragments = split_audio(job_id, 20)
+        print(f"Split into {len(fragments)} fragments")
+
+        # Create a list of tasks
+        tasks = [transcribe_fragment(job_id, i, fragment, prompt) for i, fragment in enumerate(fragments)]
+
+        # Run tasks concurrently
+        results = await asyncio.gather(*tasks)
+
+        result = merge_json_objects(results)
+    else:
+        with open(job_folder_path + '/' + job_id + '.' + input_format, 'rb') as file:
+            verbose_answer = await openai.Audio.atranscribe("whisper-1", file, response_format="verbose_json", prompt=prompt)
+
+        result = {
+            "segments": []
+        }
+
+        # iterate over segments, get only start and text
+        for segment in verbose_answer["segments"]:
+            result["segments"].append({
+                "start": segment["start"],
+                "text": segment["text"]
+            })
+
+    return result
+
+
+
+
+async def transcribe_fragment(job_id, i, fragment, prompt=""):
+    fragment_path = folder_path + '/' + job_id + '_' + str(i) + '.mp3'
+    fragment.export(fragment_path, format='mp3')
+    fragment_result = await openai.Audio.atranscribe("whisper-1", fragment_path, prompt=prompt)
+    os.remove(fragment_path)
+    return fragment_result
+
+async def process_vtt(file_path, prompt="", response_format="vtt", start_with=1, max_size=25):
     job_id = os.path.splitext(os.path.basename(file_path))[0]
 
     job_folder_path = folder_path + os.path.splitext(os.path.basename(file_path))[0]
@@ -125,15 +194,13 @@ async def process_audio(file_path, prompt="", response_format="vtt", start_with=
         file_path_to_transcribe = await convert_media_to_mp3(file_path, job_folder_path + '/' + job_id + '.mp3')
 
     file_size = get_file_size(file_path_to_transcribe)
-
     print(f"File size of {file_path_to_transcribe} is {file_size} MB")
-
     if file_size < max_size:
         print(f"Transcribing job {job_id}... Full size")
         # await transcribe_and_save(file_path_to_transcribe, output_path, 'vtt', 0, 1)
         with open(file_path_to_transcribe, 'rb') as audio_file:
             transcript = await openai.Audio.atranscribe("whisper-1", audio_file, prompt=prompt,
-                                                       response_format=response_format)
+                                                        response_format=response_format)
         # save string as vtt file
         with open(output_path, 'w') as f:
             f.write(transcript)
@@ -155,7 +222,7 @@ async def process_audio(file_path, prompt="", response_format="vtt", start_with=
 
             with open(audio_path, 'rb') as audio_file:
                 vtt_string = await openai.Audio.atranscribe("whisper-1", audio_file, prompt=prompt,
-                                                           response_format=response_format)
+                                                            response_format=response_format)
 
             vtt_transcript_stings.append(vtt_string)
 
@@ -174,6 +241,15 @@ async def process_audio(file_path, prompt="", response_format="vtt", start_with=
     # remove the job folder
     shutil.rmtree(job_folder_path)
     return binary
+
+
+async def process_audio(file_path, prompt="", response_format="vtt", start_with=1, max_size=25):
+    # This function creates separate folders for each job and transcribes the audio file
+
+    if response_format == "vtt":
+        return await process_vtt(file_path, prompt, response_format, start_with, max_size)
+    elif response_format == "text":
+        return await openai.Audio.atranscribe("whisper-1", file_path, prompt=prompt, response_format=response_format)
 
 
 def merge_vtt_strings(vtt_strings):
--- whisper/whisper_bot.py
+++ whisper/whisper_bot.py
@@ -3,6 +3,7 @@ import io
 import os
 from datetime import datetime, timedelta
 
+import openai
 import yaml
 from telethon import TelegramClient, events, sync
 from whisper import transcribe
@@ -15,37 +16,102 @@ with open('../config/config.yml', 'r') as config_file:
 api_key = config_data['api_id']
 api_hash = config_data['api_hash']
 
-client = TelegramClient('session_name', api_key, api_hash)
+client = TelegramClient('ruby.session', api_key, api_hash)
 
-async def transcribe_message(message, prompt=""):
-    def callback(current, total):
-        print('Downloaded', current, 'out of', total,
-              'bytes: {:.2%}'.format(current / total))
+from statemachine import StateMachine, State
 
+
+# Define our states
+class Job():
+    def __init__(self, message, output_format):
+        super().__init__()
+        self.id = str(uuid.uuid4())
+        self.output_format = output_format
+        self.message = message
+        self.job_directory = self.create_job_directory()
+        self.prompt = ""
+        self.is_large = None
+        self.fragments = None
+        self.converted_media_path = None
+        self.result = None
+
+    def create_job_directory(self):
+        job_directory = "jobs/" + self.id
+        if not os.path.exists(job_directory):
+            os.makedirs(job_directory)
+
+        return job_directory
+
+    async def download(self):
+        def callback(current, total):
+            print('Downloaded', current, 'out of', total,
+                  'bytes: {:.2%}'.format(current / total))
+
+        if self.message.voice:
+            filename = self.id + ".ogg"
+            print(f"Handling voice message: {filename}")
+            filename = await client.download_media(self.message, file=self.job_directory + "/" + filename,
+                                                   progress_callback=callback)
+            print(f"Downloaded voice message to {filename}")
+
+    async def convert(self):
+        self.converted_media_path = await transcribe.convert_media_to_mp3(self.job_directory + "/" + self.id + ".ogg")
+        return
+
+    async def check_size(self):
+        self.is_large = transcribe.is_large(self.converted_media_path)
+        return
+
+    async def process_audio(self):
+        print(f"Processing audio for job {self.id}")
+        self.result = await transcribe.process_job(job_id=self.id)
+        # if self.is_large:
+        #     self.result = await transcribe.process_audio(self.fragments, self.job_directory)
+        # else:
+        #     with open(self.converted_media_path, "rb") as file:
+        #         self.result = await openai.Audio.atranscribe("whisper-1", file, prompt=self.prompt,
+        #                                                      response_format=self.output_format)
+        return
+
+    def get_result(self):
+        print(f"Processing result for job {self.id}")
+        # need to parse the result as a single message
+        result = ""
+        for chunk in self.result["segments"]:
+            print(chunk)
+            result += chunk["text"]
+
+        return result
+
+
+
+
+async def transcribe_message(message, prompt="", format=""):
     job_id = str(uuid.uuid4())
     job_directory = "jobs/" + job_id
     # create directory in jobs/job_id
     if not os.path.exists(job_directory):
         os.makedirs(job_directory)
 
+    filename = ""
     if message.voice:
         filename = job_id + ".ogg"
         print(f"Handling voice message: {filename}")
 
         filename = await client.download_media(message, file=job_directory + "/" + filename, progress_callback=callback)
         print(f"Downloaded voice message to {filename}")
-        if os.path.exists(filename):
-            transcription = await transcribe.process_audio(filename)
-            return transcription
 
     elif message.file:
         filename = job_id + message.file.ext
         print(f"Handling file message: {filename}")
+
         filename = await client.download_media(message, file=job_directory + "/" + filename, progress_callback=callback)
         print(f"Downloaded file message to {filename}")
-        if os.path.exists(filename):
-            transcription = await transcribe.process_audio(filename)
-            return transcription
+
+    if os.path.exists(filename):
+        transcription = await transcribe.process_audio(filename, prompt=prompt, response_format=format)
+        return transcription
+
 
 # client on, when user sends a message, it will be transcribed
 @client.on(events.NewMessage(incoming=False))
@@ -60,31 +126,41 @@ async def my_event_handler(event):
     # check with regex
     to_transcribe = False
 
+    message_to_transcribe = await client.get_messages(peer_id, ids=event.message.reply_to_msg_id)
 
-    if "@transcribe_vtt_file" in event.message.message:
+    async def check_reply():
         if not event.message.reply_to_msg_id:
             await client.send_message(peer_id, "Please reply to a message to transcribe")
             return
 
+    if "@transcribe_vtt_file" in event.message.message:
+        await check_reply()
         # schema is @transcribe_vtt_file <prompt>
         prompt = event.message.message.split("@transcribe_vtt_file")[1]
+        format = "vtt"
 
-        # get the message to transcribe
-        message_to_transcribe = await client.get_messages(peer_id, ids=event.message.reply_to_msg_id)
-        bytes = await transcribe_message(message_to_transcribe)
+    elif "@transcribe" in event.message.message:
+        await check_reply()
+        prompt = event.message.message.split("@transcribe")[1]
+        format = "text"
 
-        if bytes is not None:
-            file = io.BytesIO(bytes)
-            file.name = "transcription.txt"
-            await client.send_file(peer_id, file, force_document=True)
-
-
-
+    else:
         return
 
+    job = Job(message_to_transcribe, "text")
+    await job.download()
+    await job.convert()
+    await job.check_size()
+    await job.process_audio()
+
+    if job.result:
+        await client.send_message(peer_id, prepend_message + job.get_result(), parse_mode="html")
 
 
 
+    # get the message to transcribe
+
+    return
 
     # transcription = await transcribe_message(event.message)
     # # check if it was a voice message
